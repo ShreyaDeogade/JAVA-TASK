50ae962a19432e334d780a5b6a8ff75759e18ae9
Shreya Arvind Deogade <122156219+ShreyaDeogade@users.noreply.github.com>
2024-02-14 12:15:34 +0530

    Create Random_number_game.java
    It contains the java program to guess the  random number .

---
+++ Random_number_game.java
@@ -0,0 +1,55 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomNumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int minRange = 1;
+        int maxRange = 100;
+        int attemptsLimit = 5;
+        int score = 0;
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            System.out.println("Welcome to the Random Number Game!");
+            System.out.println("Guess the number between " + minRange + " and " + maxRange + ".");
+
+            while (!guessedCorrectly && attempts < attemptsLimit) {
+                System.out.print("Enter your guess: ");
+                int guess = scanner.nextInt();
+                scanner.nextLine(); // Consume newline character
+
+                if (guess == randomNumber) {
+                    System.out.println("Congratulations! You guessed the correct number!");
+                    guessedCorrectly = true;
+                    score++;
+                } else if (guess < randomNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+
+                attempts++;
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Out of attempts! The correct number was: " + randomNumber);
+            }
+
+            System.out.println("Your score: " + score);
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playChoice = scanner.nextLine();
+            if (!playChoice.equalsIgnoreCase("yes")) {
+                playAgain = false;
+            }
+        }
+
+        System.out.println("Thank you for playing the Random Number Game!");
+        scanner.close();
+    }
+}
